dad63a880f46f9a833b072efa33886dd640c1c58
Daniel Pacheco <dapachec@gmail.com>
2022-09-30 15:58:04 +0200

    update preprocessing

--- preprocessing.m
+++ preprocessing.m
@@ -73,31 +73,144 @@ eegplot(EEG.data(chanids,:), 'srate', EEG.srate, 'eloc_file',EEG.chanlocs(chanid
 
 
 
+
+%% ONE AT THE TIME 
+
+%eeglab
+clear, close all
+path_log = '/Users/danielpacheco/Documents/iEEG_data_analysis/extinction/data/preproc/trialinfo/'
+path_edf = '/Users/danielpacheco/Documents/iEEG_data_analysis/extinction/raw_data/'
+currentPath = pwd; 
+
+sub = 'c_sub23'
+
+cd(path_log)
+log_list = dir('*mat'); log_list = {log_list.name};
+
+cd(path_edf)
+edf_list = dir(); edf_list = edf_list(3:end); iDir = [edf_list.isdir]; edf_list = edf_list(iDir);
+edf_list = {edf_list.name}';
+
+cd(path_log)
+load ([sub '_trlinfo.mat']);
+
+cd([path_edf sub '/ieeg/'])
+edf_list = dir('*edf'); edf_list = {edf_list.name};
+if length(edf_list) < 2    
+    EEG = pop_biosig(edf_list{1}, 'importevent', 'off'); 
+else
+    EEG = pop_biosig(edf_list{1}, 'importevent', 'off'); 
+    EEG2 = pop_biosig(edf_list{2}, 'importevent', 'off'); EEG.data = [EEG.data EEG2.data];
+end        
+
+clearvars -except EEG subji paths trlinfo path_log currentPath sub
+eventChannel = 'POL DC12';
+if strcmp(sub, 'c_sub09') eventChannel = 'POL DC09'; end
+
+TTL = strmatch(eventChannel, {EEG.chanlocs.labels}, 'exact');
+strEvent = 'X > 100000';
+EEG.event = [];
+EEG = pop_chanevent (EEG, TTL, 'edge', 'leading', 'oper', strEvent, 'delchan', 'off', ...
+    'delevent', 'on');
+
+factor = ( 10000 / EEG.srate); 
+x = [EEG.event.latency]';
+[d id2Break] = max(abs(diff(x)));
+
+if strcmp(sub, 'c_sub04') [ii,ii] = sort(abs(diff(x))); id2Break = ii(end-2);end
+if strcmp(sub, 'c_sub06') [ii,ii] = sort(abs(diff(x))); id2Break = ii(end-1);end
+
+lat2breakTTL = EEG.event(id2Break+1).latency;
+if strcmp(sub, 'c_sub09') lat2breakTTL = EEG.event(id2Break+10).latency; end
+if strcmp(sub, 'c_sub12') lat2breakTTL = EEG.event(id2Break+63).latency; end
+if strcmp(sub, 'c_sub15') lat2breakTTL = EEG.event(id2Break+122).latency; end
+if strcmp(sub, 'c_sub16') lat2breakTTL = EEG.event(id2Break+169).latency; end
+if strcmp(sub, 'c_sub18') lat2breakTTL = EEG.event(id2Break+151).latency; end
+if strcmp(sub, 'c_sub19') lat2breakTTL = EEG.event(id2Break+26).latency; end
+
+lat_prev = trlinfo(:, 10:14); 
+latencies = reshape (lat_prev', 1, [])'; 
+latencies(latencies == 0) = nan; 
+
+sCod = string(trlinfo(:, 1:9));
+sCod (ismissing(sCod)) = 'nan';
+typOT = ['T' 'V' 'C' 'U' 'R']';
+typOT = repmat(typOT, 192, 1);
+sCod1 = repelem(sCod, 5, 1); 
+%sCod1 = [sCod1, string(typOT)]; %uncomment to include trialinfo
+sCodF = join(sCod1, '_');
+
+[d id2Break] = max(abs(diff(latencies)));
+lat2breakLog = latencies(id2Break+2)/factor;
+if strcmp(sub, 'c_sub09') lat2breakLog = latencies(id2Break+1)/factor; end
+newBreak = lat2breakTTL - lat2breakLog
+
+lat2u = 1; 
+if strcmp(sub, 'c_sub06') lat2u = 58; end
+if strcmp(sub, 'c_sub09') lat2u = 11; end
+lat = EEG.event(lat2u).latency; 
+EEG.event = struct('latency', [], 'type', '', 'urevent', []);
+diff_12 =  latencies(2) / factor - lat;
+
+for i = 1:id2Break
+    EEG.event(i) = struct('latency', latencies(i)/factor -diff_12 , 'type', sCodF(i,:), 'urevent', 0);
+end
+for i = id2Break+1:length(sCodF)
+     EEG.event(i) = struct('latency', latencies(i)/factor + newBreak , 'type',sCodF(i,:), 'urevent', 0);
+end
+
+%EEG.event = nestedSortStruct(EEG.event, 'latency');
+cd(currentPath);
+
+
+
+%%
+
+eventChannel = 'POL DC12';if strcmp(sub ,'c_sub09') eventChannel = 'POL DC09'; end
+TTL = strmatch(eventChannel, {EEG.chanlocs.labels}, 'exact');chanids = [TTL];
+eegplot(EEG.data(chanids,:), 'srate', EEG.srate, 'eloc_file',EEG.chanlocs(chanids), ...
+    'winlength', 50, 'spacing', 10000000, 'events', EEG.event);
+
+
+
 %% ALL IN LOOP
 
 %eeglab
 clear, close all
 path_log = '/Users/danielpacheco/Documents/iEEG_data_analysis/extinction/data/preproc/trialinfo/'
-path_edf = '/Users/danielpacheco/Documents/iEEG_data_analysis/extinction/data/raw_data/'
+path_edf = '/Users/danielpacheco/Documents/iEEG_data_analysis/extinction/raw_data/'
 currentPath = pwd; 
 
-for subji = 21:21
+allsubs = {'c_sub01','c_sub02','c_sub03','c_sub04','c_sub05','c_sub06','c_sub07','c_sub08', ...
+           'c_sub09','c_sub10','c_sub11','c_sub12','c_sub13','c_sub14','c_sub15','c_sub16', ...
+           'c_sub17','c_sub18', 'c_sub19','c_sub20', 'c_sub22', 'c_sub23','c_sub24', ...
+            'c_sub25','c_sub26','c_sub29' };
 
+cd(path_log)
+log_list = dir('*mat'); log_list = {log_list.name};
+
+cd(path_edf)
+edf_list = dir(); edf_list = edf_list(3:end); iDir = [edf_list.isdir]; edf_list = edf_list(iDir);
+edf_list = {edf_list.name}';
+       
+for subji = 1:length(allsubs)
+
+    sub = allsubs{subji};
     cd(path_log)
-    log_list = dir('*mat'); log_list = {log_list.name};
-    load (log_list{subji});
-    if subji < 10 cd([path_edf 'c_sub0' num2str(subji) '/ieeg/']); end 
-    if subji >= 10 cd([path_edf 'c_sub' num2str(subji) '/ieeg/']); end 
-    edf_list = dir('*edf'); edf_list = {edf_list.name};
-    EEG = pop_biosig(edf_list{1}, 'importevent', 'off'); 
-    if subji == 19 EEG2 = pop_biosig(edf_list{2}, 'importevent', 'off'); EEG.data = [EEG.data EEG2.data];end
-    if subji == 22 EEG2 = pop_biosig(edf_list{2}, 'importevent', 'off'); EEG.data = [EEG.data EEG2.data ];end
-        
-
+    load ([sub '_trlinfo.mat']);
     
-    clearvars -except EEG subji paths trlinfo path_log currentPath
+    cd([path_edf sub '/ieeg/'])
+    edf_list = dir('*edf'); edf_list = {edf_list.name};
+    if length(edf_list) < 2 
+        EEG = pop_biosig(edf_list{1}, 'importevent', 'off'); 
+    else
+        EEG = pop_biosig(edf_list{1}, 'importevent', 'off'); 
+        EEG2 = pop_biosig(edf_list{2}, 'importevent', 'off'); EEG.data = [EEG.data EEG2.data];
+    end        
+
+    clearvars -except EEG subji paths trlinfo path_log currentPath sub
     eventChannel = 'POL DC12';
-    if subji == 9 eventChannel = 'POL DC09'; end
+    if strcmp(sub, 'c_sub09') eventChannel = 'POL DC09'; end
     
     TTL = strmatch(eventChannel, {EEG.chanlocs.labels}, 'exact');
     strEvent = 'X > 100000';
@@ -109,16 +222,16 @@ for subji = 21:21
     x = [EEG.event.latency]';
     [d id2Break] = max(abs(diff(x)));
     
-    if subji == 4 [ii,ii] = sort(abs(diff(x))); id2Break = ii(end-2);end
-    if subji == 6 [ii,ii] = sort(abs(diff(x))); id2Break = ii(end-1);end
+    if strcmp(sub, 'c_sub04') [ii,ii] = sort(abs(diff(x))); id2Break = ii(end-2);end
+    if strcmp(sub, 'c_sub06') [ii,ii] = sort(abs(diff(x))); id2Break = ii(end-1);end
     
     lat2breakTTL = EEG.event(id2Break+1).latency;
-    if subji == 9 lat2breakTTL = EEG.event(id2Break+10).latency; end
-    if subji == 12 lat2breakTTL = EEG.event(id2Break+63).latency; end
-    if subji == 15 lat2breakTTL = EEG.event(id2Break+122).latency; end
-    if subji == 16 lat2breakTTL = EEG.event(id2Break+169).latency; end
-    if subji == 18 lat2breakTTL = EEG.event(id2Break+151).latency; end
-    if subji == 19 lat2breakTTL = EEG.event(id2Break+26).latency; end
+    if strcmp(sub, 'c_sub09') lat2breakTTL = EEG.event(id2Break+10).latency; end
+    if strcmp(sub, 'c_sub12') lat2breakTTL = EEG.event(id2Break+63).latency; end
+    if strcmp(sub, 'c_sub15') lat2breakTTL = EEG.event(id2Break+122).latency; end
+    if strcmp(sub, 'c_sub16') lat2breakTTL = EEG.event(id2Break+169).latency; end
+    if strcmp(sub, 'c_sub18') lat2breakTTL = EEG.event(id2Break+151).latency; end
+    if strcmp(sub, 'c_sub19') lat2breakTTL = EEG.event(id2Break+26).latency; end
     
     lat_prev = trlinfo(:, 10:14); 
     latencies = reshape (lat_prev', 1, [])'; 
@@ -134,12 +247,12 @@ for subji = 21:21
     
     [d id2Break] = max(abs(diff(latencies)));
     lat2breakLog = latencies(id2Break+2)/factor;
-    if subji == 9 lat2breakLog = latencies(id2Break+1)/factor; end
+    if strcmp(sub, 'c_sub09') lat2breakLog = latencies(id2Break+1)/factor; end
     newBreak = lat2breakTTL - lat2breakLog
 
     lat2u = 1; 
-    if subji == 6 lat2u = 58; end
-    if subji == 9 lat2u = 11; end
+    if strcmp(sub, 'c_sub06') lat2u = 58; end
+    if strcmp(sub, 'c_sub09') lat2u = 11; end
     lat = EEG.event(lat2u).latency; 
     EEG.event = struct('latency', [], 'type', '', 'urevent', []);
     diff_12 =  latencies(2) / factor - lat;
@@ -161,7 +274,7 @@ disp ('done');
 
 %%
 
-eventChannel = 'POL DC12';if subji == 9 eventChannel = 'POL DC09'; end
+eventChannel = 'POL DC12';if strcmp(sub ,'c_sub09') eventChannel = 'POL DC09'; end
 TTL = strmatch(eventChannel, {EEG.chanlocs.labels}, 'exact');chanids = [TTL];
 eegplot(EEG.data(chanids,:), 'srate', EEG.srate, 'eloc_file',EEG.chanlocs(chanids), ...
     'winlength', 50, 'spacing', 10000000, 'events', EEG.event);
