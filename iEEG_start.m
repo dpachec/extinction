4af4f5159668cc7306165eabaf57c3389cc484d9
Daniel Pacheco <dapachec@gmail.com>
2022-11-09 17:40:06 +0100

    Update iEEG_start.m

--- iEEG_start.m
+++ iEEG_start.m
@@ -4,7 +4,7 @@ clear, close all
 paths = load_paths; 
 
 c2u = 'C';
-sROI = 'Amygdala';
+sROI = {'middlefrontal'};
 
 allsubs = {'c_sub01','c_sub02','c_sub03','c_sub04','c_sub05','c_sub06','c_sub07','c_sub08', ...
            'c_sub09','c_sub10','c_sub11','c_sub12','c_sub13','c_sub14','c_sub15','c_sub16', ...
@@ -74,6 +74,8 @@ for subji = 1:length(allsubs)
 
         %remove amplitude data 
         EEG = rmfield(EEG, 'data');
+        EEG = rmfield(EEG, 'markers_artifacts');
+        EEG = rmfield(EEG, 'powerDS');
         EEG = normalize_EXT(EEG);
     
         ALLEEG{subji,:} = EEG; 
@@ -82,13 +84,14 @@ for subji = 1:length(allsubs)
 
 end
 
+sROI = char(join(sROI, '_'));
 filename = [paths.iEEGRes.power 'allS_' sROI '_' c2u];
 save(filename, "ALLEEG");
 
 cd (paths.github)
 
 %% check that markers are ok
-data2check = [EEG.data(1, :); EEG.marker_artifacts(1,:)*1000]; 
+data2check = [EEG.data(1, :); EEG.markers_artifacts(1,:)*1000]; 
 eegplot(data2check, 'srate', EEG.srate, 'winlength', 50, 'spacing', 1000);
 
 
@@ -154,7 +157,7 @@ contourf(1:701, 1:54, d2p2, 40, 'linecolor', 'none'); hold on; %colorbar
 
 %% PLOT grand average for each condition
 paths = load_paths; 
-file2load = 'allS_Amygdala_C'; 
+file2load = 'allS_middlefrontal_C'; 
 %load ([paths.iEEGRes.power file2load]); 
 clearvars -except ALLEEG paths file2load
 
@@ -203,12 +206,19 @@ sub2exc = []
 c1B = c1; c2B = c2; 
 c1B(sub2exc,:,:) = []; c2B(sub2exc,:,:) = []; 
 
+c1B(c1B == 0) = nan; 
+c2B(c2B == 0) = nan; 
 d2p1	= squeeze(mean(c1B, 'omitnan'));
 d2p2	= squeeze(mean(c2B, 'omitnan'));
 
 
 [h p ci ts] = ttest(c1, c2); 
 h = squeeze(h); t = squeeze(ts.tstat);
+
+%h = zeros(54, 300);
+%h(clustinfo.PixelIdxList{45}) = 1; 
+
+
 
 times = -1:.01:1.99; 
 tiledlayout(3, 1,'TileSpacing','loose'); set(gcf, 'Position', [100 100 600 800])
@@ -227,22 +237,90 @@ exportgraphics(gcf, [paths.iEEGRes.power file2load '.png'], 'Resolution',150)
 
 
 %% stats 
+clear allSTs  
+clustinfo = bwconncomp(h);
+for pxi = 1:length(clustinfo.PixelIdxList)
+   allSTs(pxi,:) = sum(t(clustinfo.PixelIdxList{pxi}));% 
+end
+[max2u id] = max(abs(allSTs));
+max_clust_obs = allSTs(id); 
+
+
+%% permutations 
+
+nPerm = 1000; 
+clear max_clust_sum_perm
+for permi = 1:nPerm
+    c1B = c1(:,:,101:300); 
+    c2B = c2(:,:,101:300); 
+    c1B(c1B == 0) = nan; 
+    c2B(c2B == 0) = nan; 
+    for subji = 1:size(c1B, 1)
+        if rand>.5
+           tmp = c1B(subji, :, :);
+           c1B(subji, :, :) = c2B(subji, :, :);
+           c2B(subji, :, :) = tmp; 
+        end
+    end
+    
+    CP1	= squeeze(mean(c1B, 'omitnan'));
+    CP2	= squeeze(mean(c2B, 'omitnan'));
+    [hPerm p ci tsPerm] = ttest(CP1, CP2); 
+    hPerm = squeeze(hPerm); tPerm = squeeze(tsPerm.tstat);
+
+    clear allSTs  
+    clustinfo = bwconncomp(hPerm);
+    for pxi = 1:length(clustinfo.PixelIdxList)
+        allSTs(pxi,:) = sum(tPerm(clustinfo.PixelIdxList{pxi}));% 
+    end
+    if exist('allSTs')
+        [max2u id] = max(abs(allSTs));
+    else
+        allSTs = 0; 
+    end
+    max_clust_sum_perm(permi,:) = abs(allSTs(id)); 
+
+end
+
+%%
+clear p mcsR mcsP
+
+mcsR = max_clust_obs; 
+mcsP = max_clust_sum_perm;
+
+allAb = mcsP(abs(mcsP) > abs(mcsR))';
+p = 1 - ((nPerm-1) - (length (allAb)))  / nPerm;
+
+
+
+
+%% 
+figure
+histogram(max_clust_sum_perm); hold on; 
+scatter(abs(max_clust_obs),0, 'filled','r');
+set(gca, 'FontSize', 14);
+xlabel('T')
+exportgraphics(gcf, [paths.iEEGRes.power 'myP.png'], 'Resolution',150)
 
 
 
 
 
+%% count channels 
+
+clear nChans
+for subji = 1:length(ALLEEG)
+
+    EEG = ALLEEG{subji};
+    if~isempty(EEG)
+        nChans(subji,:) = length(EEG.chanlocs);
+    end
 
 
+end
 
-
-
-
-
-
-
-
-
+disp (['Subjects with elec: ' num2str(length(find(nChans)))])
+disp (['Total elec num: ' num2str(sum(nChans))])
 
 
 
