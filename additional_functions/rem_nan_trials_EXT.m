17142fc45527d7b35f2881b88d295f914437c0d2
Daniel Pacheco <dapachec@gmail.com>
2023-10-09 15:15:16 +0200

    removing nan EXT

---
+++ additional_functions/rem_nan_trials_EXT.m
@@ -0,0 +1,18 @@
+function[EEG] = rem_nan_trials_EXT (EEG)
+    for triali = 1:size(EEG.data, 3)
+        count = 1; 
+        for triali = 1:size(EEG.data, 3)
+            dtr = squeeze(EEG.data(:, :,triali)); 
+            if find(isnan(dtr))
+                id2rem(count,:) = triali; 
+                count = count+1;
+            end
+        end
+    end
+    if exist('id2rem')
+        disp (['removing ' num2str(length(id2rem)) ' trials'])
+        EEG.data(:, :, id2rem) = []; 
+        EEG.event(id2rem) = []; 
+    end
+
+end
