f238c0bb64ff016370bc460e6867c1b240ada6ba
Daniel Pacheco <dapachec@gmail.com>
2023-10-09 14:52:05 +0200

    update region names

--- additional_functions/loadTracesEXT.m
+++ additional_functions/loadTracesEXT.m
@@ -3,35 +3,35 @@ function [ALLEEG] = loadTracesEXT(roi, LT, paths)
 
     if strcmp(roi, 'AMY')
         if strcmp(LT, 'V')
-            file2load = ['TR_' 'Amygdala' '_V']; 
+            file2load = ['TR_' 'AMY' '_V']; 
         elseif strcmp(LT, 'C')
-            file2load = ['TR_' 'Amygdala' '_C']; 
+            file2load = ['TR_' 'AMY' '_C']; 
         end
     elseif strcmp(roi, 'HPC')
         if strcmp(LT, 'V')
-            file2load = ['TR_' 'Hippocampus' '_V']; 
+            file2load = ['TR_' 'HPC' '_V']; 
         elseif strcmp(LT, 'C')
-            file2load = ['TR_' 'Hippocampus' '_C']; 
+            file2load = ['TR_' 'HPC' '_C']; 
         end
         
     elseif strcmp(roi, 'OFC')
         if strcmp(LT, 'V')
-            file2load = ['TR_' 'orbitofrontal' '_V']; 
+            file2load = ['TR_' 'OFC' '_V']; 
         elseif strcmp(LT, 'C')
-            file2load = ['TR_' 'orbitofrontal' '_C']; 
+            file2load = ['TR_' 'OFC' '_C']; 
         end
     
     elseif strcmp(roi, 'VVS')
         if strcmp(LT, 'V')
-            file2load = ['TR_' 'VVS' '_V']; 
+            file2load = ['TR_' 'TMP' '_V']; 
         elseif strcmp(LT, 'C')
-            file2load = ['TR_' 'VVS' '_C']; 
+            file2load = ['TR_' 'TMP' '_C']; 
         end
     elseif strcmp(roi, 'OCC')
         if strcmp(LT, 'V')
-            file2load = ['TR_' 'occipital' '_V']; 
+            file2load = ['TR_' 'OCC' '_V']; 
         elseif strcmp(LT, 'C')
-            file2load = ['TR_' 'occipital' '_C']; 
+            file2load = ['TR_' 'OCC' '_C']; 
         end
     
     end 
