946b7eb4fd5cbd9ae2f6d7e96834f1ed70b44b64
Daniel Pacheco <dapachec@gmail.com>
2023-10-12 11:34:08 +0200

    1

--- rsa_phase_EXT.m
+++ rsa_phase_EXT.m
@@ -4,8 +4,14 @@
 clear , clc
 
 listF2sav = {
-
                 'PHA_OCC_V_3-8_0_0_50-10_1_SC-DC';
+                'PHA_OCC_V_9-12_0_0_50-10_1_SC-DC';
+                'PHA_OCC_V_13-29_0_0_50-10_1_SC-DC';
+                'PHA_OCC_V_30-54_0_0_50-10_1_SC-DC';
+                'PHA_OCC_V_30-38_0_0_50-10_1_SC-DC';
+                'PHA_OCC_V_39-54_0_0_50-10_1_SC-DC';
+                
+                
 
         };   
 
@@ -193,7 +199,7 @@ p = 1 - ((nPerm-1) - (length (allAb)))  / nPerm
 %% plot TG
 clear
 paths = load_paths_EXT; 
-f2sav = 'PHA_OCC_V_3-8_0_0_50-10_1_SC-DC';
+f2sav = 'PHA_OCC_V_3-54_0_0_50-10_1_SC-DC';
  
 
 load ([ paths.results.rsa f2sav '.mat']);
