b3d41c4de759bc0ad6e7404395aacac8767d13e3
Daniel Pacheco <dapachec@gmail.com>
2023-11-28 17:08:13 +0100

    update load traces

--- additional_functions/loadTracesEXT.m
+++ additional_functions/loadTracesEXT.m
@@ -3,47 +3,47 @@ function [ALLEEG] = loadTracesEXT(roi, LT, paths)
 
     if strcmp(roi, 'AMY')
         if strcmp(LT, 'V')
-            file2load = ['TR_' 'AMY' '_V_6_4']; 
+            file2load = ['TR_' 'AMY' '_V']; 
         elseif strcmp(LT, 'C')
-            file2load = ['TR_' 'AMY' '_C_6_4']; 
+            file2load = ['TR_' 'AMY' '_C']; 
         end
     elseif strcmp(roi, 'HPC')
         if strcmp(LT, 'V')
-            file2load = ['TR_' 'HPC' '_V_6_4']; 
+            file2load = ['TR_' 'HPC' '_V']; 
         elseif strcmp(LT, 'C')
-            file2load = ['TR_' 'HPC' '_C_6_4']; 
+            file2load = ['TR_' 'HPC' '_C']; 
         end
         
     elseif strcmp(roi, 'OFC')
         if strcmp(LT, 'V')
-            file2load = ['TR_' 'OFC' '_V_6_4']; 
+            file2load = ['TR_' 'OFC' '_V']; 
         elseif strcmp(LT, 'C')
-            file2load = ['TR_' 'OFC' '_C_6_4']; 
+            file2load = ['TR_' 'OFC' '_C']; 
         end
     
     elseif strcmp(roi, 'TMP')
         if strcmp(LT, 'V')
-            file2load = ['TR_' 'TMP' '_V_6_4']; 
+            file2load = ['TR_' 'TMP' '_V']; 
         elseif strcmp(LT, 'C')
-            file2load = ['TR_' 'TMP' '_C_6_4']; 
+            file2load = ['TR_' 'TMP' '_C']; 
         end
     elseif strcmp(roi, 'OCC')
         if strcmp(LT, 'V')
-            file2load = ['TR_' 'OCC' '_V_6_4']; 
+            file2load = ['TR_' 'OCC' '_V']; 
         elseif strcmp(LT, 'C')
-            file2load = ['TR_' 'OCC' '_C_6_4']; 
+            file2load = ['TR_' 'OCC' '_C']; 
         end
     elseif strcmp(roi, 'FRO')
         if strcmp(LT, 'V')
-            file2load = ['TR_' 'FRO' '_V_6_4']; 
+            file2load = ['TR_' 'FRO' '_V']; 
         elseif strcmp(LT, 'C')
-            file2load = ['TR_' 'FRO' '_C_6_4']; 
+            file2load = ['TR_' 'FRO' '_C']; 
         end    
     elseif strcmp(roi, 'PFC')
         if strcmp(LT, 'V')
-            file2load = ['TR_' 'PFC' '_V_6_4']; 
+            file2load = ['TR_' 'PFC' '_V']; 
         elseif strcmp(LT, 'C')
-            file2load = ['TR_' 'PFC' '_C_6_4']; 
+            file2load = ['TR_' 'PFC' '_C']; 
         end  
     
     end 
